2ffc0f2f60c00e2668a696998c428963fdee5bb0
helder-hanka <hfernandes238@gmail.com>
2021-03-16 19:31:53 +0100

    import Navbar, Footer, AddBooks, SearchBooks and add React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch,  Route} from 'react-router-dom';
+import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
+import AddBooks from './Containers/AddBooks'
+import SearchBooks from './Containers/SearchBooks'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+          <Switch>
+              <Route exact path="/" component={ AddBooks } />
+              <Route path="/search" component={ SearchBooks } />     
+          </Switch>
+        <Footer /> 
+      </Router>
     </div>
   );
 }
